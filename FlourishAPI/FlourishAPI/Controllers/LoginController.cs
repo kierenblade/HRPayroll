5974f20e2d52b84a93b8407b7422c326fb9bd1a3
Tshief <tthifularo@gmail.com>
2018-01-25 12:29:37 +0200

    Added new "FlourishAPI"

--- FlourishAPI/FlourishAPI/Controllers/LoginController.cs
+++ FlourishAPI/FlourishAPI/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FlourishAPI.DTOs;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,30 +26,36 @@ namespace FlourishAPI.Controllers
             return "Authenticate";
         }
 
-        // GET api/login/AlternativeGet
-        [HttpGet("AlternativeGet")] // <-- Specify your own Method name
-        public string AltGet(int id)
-        {
-            return "Alternate Get";
-        }
-
         // POST api/login
         [HttpPost]
-        public string Post([FromBody]string value)
+        public Boolean Post([FromBody]string username, string password)
         {
-            return "We received " + value;
+            bool successfull = false;
+
+            return successfull;
         }
 
-        // PUT api/login/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        
 
-        // DELETE api/login/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        // GET api/login/AlternativeGet
+        //[HttpGet("AlternativeGet")] // <-- Specify your own Method name
+        //public string AltGet(int id)
+        //{
+        //    return "Alternate Get";
+        //}
+
+
+
+
+
+
+
+
+
+        //// DELETE api/login/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
---
+++ FlourishAPI/FlourishAPI/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlourishAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Report")]
+    public class ReportController : Controller
+    {
+
+        // PUT api/Report/GenReportRequest
+        [HttpPost("GenReportRequest")]
+        public string GeneralReportRequest(int id, [FromBody] string request)
+        {
+            string reportResponseJSON = "";
+
+            return reportResponseJSON;
+        }
+
+        
+    }
+}
