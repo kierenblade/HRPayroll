45970040632f1b19135de3d3c34a350897fed12c
KGerling <kieren.gerling@sybrin.co.za>
2018-01-25 20:53:32 +0200

    Added Scheduler project + updated gitignore file

---
+++ HRPayroll.Scheduler/HRPayroll.Scheduler/EmployeeSync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentScheduler;
+
+namespace HRPayroll.Scheduler
+{
+    class EmployeeSync : IJob
+    {
+        public EmployeeSync()
+        {
+            
+        }
+        public void Execute()
+        {
+            Console.WriteLine("Sync started");
+        }
+
+        public void SyncEmployeesFromClient()
+        {
+
+        }
+    }
+}
