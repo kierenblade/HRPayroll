baed399e6828ce82c16bd1134eeb31e0ee375fe7
Tshief <tthifularo@gmail.com>
2018-01-30 19:05:40 +0200

    added ClientAPI2

---
+++ ClientAPI2/ClientAPI2/Controllers/ClientDataController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FlourishAPI.Models.Classes;
+using FlourishAPI.Models;
+
+
+namespace ClientAPI2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ClientData")]
+    public class ClientDataController : Controller
+    {
+        [HttpGet]
+        public string CheckStatus()
+        {
+
+            return "You are connectec to Us";
+
+        }
+
+        [HttpGet("SyncAllEmployees")]
+
+        public IEnumerable<Employee> SyncAllEmployees()
+        {
+
+            Employee e = new Employee();
+            e.InsertDocument("ClientDB");
+            List<Employee> emps = e.GetAllEmployees("ClientDB");
+            emps.Remove(e);
+            e.Delete("ClientDB");
+
+            return emps;
+
+        }
+
+        [HttpPost("SyncEmployees")]
+        public IEnumerable<Employee> SyncEmployees(string[] empIDs)
+        {
+
+            Employee e = new Employee();
+            e.InsertDocument("ClientDB");
+            List<Employee> emps = e.GetAllEmployees("ClientDB");
+            emps.Remove(e);
+            e.Delete("ClientDB");
+
+            return emps.Where(x => x.EmployeeStatus == EmployeeStatus.Employed).ToList();
+
+        }
+
+    }
+}
